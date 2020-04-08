837ef418fef7c2e8ba6e2a9ee2fa5bcd1b392e2c
Chisato Murakami <kiwipom1031@gmail.com>
2020-04-08 13:20:46 -0700

    Add methods for rolling a dice and call functions in front end logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,63 @@
-
-
-
-
-
-var Chee = new player(chee);
-Chee.addScore();
-var chris = new player(Chris);
-Chris.addScore();
-
-//player 1 scores var
+//player constructor
 function player (id){
-  this.totalScore = 0;
-  this.turnScore = 0; 
+  this.totalScore = 0; //permanent
+  this.turnScore = 0;  //temporary 
   this.id = id;
-}
+} 
+//modifications to players
 player.prototype.addScore = function(){
   this.totalScore += this.turnScore;
   this.turnScore = 0;
 }
 
-var switchTurns(){
-  "(ThisTurn'sPlayer).addScore()"
-  "Switch Active Player's turn"
 
+//  List Of Players = [First Player, Second Player]
+var playerTurn = 0; 
+var allPlayers = [];
+allPlayers.push(new player(0))
+allPlayers.push(new player(1))
+
+// referee neutral
+function switchTurns() {
+
+  if (playerTurn === 0){
+    allPlayers[0].addScore();
+    if (allPlayers[0].totalScore >= 100) {
+      console.log("allPlayer[0], You won the game!")
+    }
+    playerTurn = 1;
+  }
+
+  else {
+    allPlayers[1].addScore();
+    if (allPlayers[1].totalScore >= 100) {
+      console.log("allPlayer[1], You won the game!")
+    }
+    playerTurn = 0;
+  }
 } 
+
+
+//   ^ End Your Turn ^
+//################################################################
+//   V Roll Your Dice V
+
+function diceRoll() {
+  var random = Math.floor(Math.random() * 6) + 1;
+  if (random == 1){
+    allPlayers[playerTurn].turnScore = 0;
+    switchTurns();
+  }
+  else {
+    allPlayers[playerTurn].turnScore += random; //YOU update the score
+//return random; //give ME the number, and I'll do something with it somewhere else
+  }
+}
+
 
 //##################################################################
 
 $(document).ready(function() {
-
-  $("#roll").click()
+  $("#roll").click(diceRoll());
+  $("#pass").click(switchTurns());
 });
